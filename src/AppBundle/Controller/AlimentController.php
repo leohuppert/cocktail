565cfb03fd6bcc717f7c1c81286ebd9249fb366a
Léo Huppert <leo.huppert57@gmail.com>
2017-10-12 11:19:03 +0200

    Add Breadcrumb on Aliment/show

--- src/AppBundle/Controller/AlimentController.php
+++ src/AppBundle/Controller/AlimentController.php
@@ -62,15 +62,32 @@ class AlimentController extends Controller
      *
      * @Route("/{id}", name="aliment_show")
      * @Method("GET")
+     * @param Aliment $aliment
+     * @return \Symfony\Component\HttpFoundation\Response
      */
     public function showAction(Aliment $aliment)
     {
-        $deleteForm = $this->createDeleteForm($aliment);
+        $superAliments = $this->getSuperAliments($aliment);
 
         return $this->render('aliment/show.html.twig', array(
-            'aliment' => $aliment,
-            'delete_form' => $deleteForm->createView(),
+            'aliment'        => $aliment,
+            'super_aliments' => array_reverse($superAliments),
         ));
+    }
+
+    /**
+     * @param Aliment $aliment
+     * @param array $res
+     * @return array
+     */
+    private function getSuperAliments(Aliment $aliment, array $res = array())
+    {
+        if ($aliment->getSuperAliments()->count() === 0) {
+            return $res;
+        }
+
+        $res[] = $aliment->getSuperAliments()[0];
+        return $this->getSuperAliments($aliment->getSuperAliments()[0], $res);
     }
 
     /**
