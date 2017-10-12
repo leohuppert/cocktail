a3a0498932e7c0f07f5f422df1fc8babd9c026ce
Léo Huppert <leo.huppert57@gmail.com>
2017-10-12 11:28:43 +0200

    Remove useless functions

--- src/AppBundle/Controller/AlimentController.php
+++ src/AppBundle/Controller/AlimentController.php
@@ -32,32 +32,6 @@ class AlimentController extends Controller
     }
 
     /**
-     * Creates a new aliment entity.
-     *
-     * @Route("/new", name="aliment_new")
-     * @Method({"GET", "POST"})
-     */
-    public function newAction(Request $request)
-    {
-        $aliment = new Aliment();
-        $form = $this->createForm('AppBundle\Form\AlimentType', $aliment);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $em = $this->getDoctrine()->getManager();
-            $em->persist($aliment);
-            $em->flush();
-
-            return $this->redirectToRoute('aliment_show', array('id' => $aliment->getId()));
-        }
-
-        return $this->render('aliment/new.html.twig', array(
-            'aliment' => $aliment,
-            'form' => $form->createView(),
-        ));
-    }
-
-    /**
      * Finds and displays a aliment entity.
      *
      * @Route("/{id}", name="aliment_show")
@@ -88,66 +62,5 @@ class AlimentController extends Controller
 
         $res[] = $aliment->getSuperAliments()[0];
         return $this->getSuperAliments($aliment->getSuperAliments()[0], $res);
-    }
-
-    /**
-     * Displays a form to edit an existing aliment entity.
-     *
-     * @Route("/{id}/edit", name="aliment_edit")
-     * @Method({"GET", "POST"})
-     */
-    public function editAction(Request $request, Aliment $aliment)
-    {
-        $deleteForm = $this->createDeleteForm($aliment);
-        $editForm = $this->createForm('AppBundle\Form\AlimentType', $aliment);
-        $editForm->handleRequest($request);
-
-        if ($editForm->isSubmitted() && $editForm->isValid()) {
-            $this->getDoctrine()->getManager()->flush();
-
-            return $this->redirectToRoute('aliment_edit', array('id' => $aliment->getId()));
-        }
-
-        return $this->render('aliment/edit.html.twig', array(
-            'aliment' => $aliment,
-            'edit_form' => $editForm->createView(),
-            'delete_form' => $deleteForm->createView(),
-        ));
-    }
-
-    /**
-     * Deletes a aliment entity.
-     *
-     * @Route("/{id}", name="aliment_delete")
-     * @Method("DELETE")
-     */
-    public function deleteAction(Request $request, Aliment $aliment)
-    {
-        $form = $this->createDeleteForm($aliment);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $em = $this->getDoctrine()->getManager();
-            $em->remove($aliment);
-            $em->flush();
-        }
-
-        return $this->redirectToRoute('aliment_index');
-    }
-
-    /**
-     * Creates a form to delete a aliment entity.
-     *
-     * @param Aliment $aliment The aliment entity
-     *
-     * @return \Symfony\Component\Form\Form The form
-     */
-    private function createDeleteForm(Aliment $aliment)
-    {
-        return $this->createFormBuilder()
-            ->setAction($this->generateUrl('aliment_delete', array('id' => $aliment->getId())))
-            ->setMethod('DELETE')
-            ->getForm()
-        ;
     }
 }
